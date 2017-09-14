5fac60dbe4e6720df3e57573812d629405193766
Geoff Brimhall <glbrimhall@dockerepo.library.arizona.edu>
2017-09-14 10:35:13 -0700

    Seperated out dcreate-registry.sh into dcreate-registry-cache.sh on port 443 and dcreate-registry-upload.sh on 5000

--- dcreate-registry-upload.sh
+++ dcreate-registry-upload.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-NAME=${1:-docker-registry-2.6}
+NAME=${1:-docker-registry-upload-2.6}
 REPOSITORY=registry:2.6
 
 # Required host environment for docker image
-HOSTPORT=443
-GUESTPORT=443
-HOSTCERTS=/repo1/registry-certs
-HOSTREPO=/repo1/registry
+HOSTPORT=5000
+GUESTPORT=5000
+HOSTCERTS=/repo1/docker/registry/certs
+HOSTREPO=/repo1/docker/registry/upload
 GUESTREPO=/var/lib/registry
 
 # !! IMPORTANT !! docker matches on the CN 
@@ -30,18 +30,16 @@ docker pull $REPOSITORY
 docker run $DAEMONIZE \
   --restart=always \
   -v $HOSTCERTS:/certs \
-  -v $HOSTCERTS:/auth \
   -e REGISTRY_HTTP_ADDR=0.0.0.0:$GUESTPORT \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/$DOMAIN.crt \
   -e REGISTRY_HTTP_TLS_KEY=/certs/$DOMAIN.key \
-  -e "REGISTRY_AUTH=htpasswd" \
-  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
-  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/users.conf \
   -p $HOSTPORT:$GUESTPORT \
   -v $HOSTREPO:$GUESTREPO \
   --name $NAME \
   $REPOSITORY
 
-
-
-#docker run -e "REGISTRY_USER=$REGISTRY_USER" -e "REGISTRY_PASSWORD=$REGISTRY_PASSWORD" -e PGDATA=$GUESTREPO -v $HOSTREPO:$GUESTREPO --net=bridge $DAEMONIZE --name $NAME -i $REPOSITORY
+#  -e REGISTRY_PROXY_REMOTEURL=https://registry-1.docker.io \
+#  -v $HOSTCERTS:/auth \
+#  -e "REGISTRY_AUTH=htpasswd" \
+#  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+#  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/users.conf \
