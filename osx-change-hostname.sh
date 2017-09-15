0cce64ddc14a2a5c32d05760f52d6ef0822f4145
Geoffrey L. Brimhall <geoff@glbrimhall.com>
2017-09-15 12:07:46 -0700

    Added script to change osx hostname

---
+++ osx-change-hostname.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DOMAIN_NAME="$1"
+MACHINE_NAME=${DOMAIN_NAME%%.*}
+
+echo "SETTING DOMAIN_NAME=$DOMAIN_NAME"
+echo "SETTING MACHINE_NAME=$MACHINE_NAME"
+
+scutil --set HostName $DOMAIN_NAME
+scutil --set LocalHostName $MACHINE_NAME.local
+scutil --set ComputerName $MACHINE_NAME
+dscacheutil -flushcache
