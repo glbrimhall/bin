82766c1510d485539796e0550886bfd7f9ab0efb
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2017-10-02 09:12:10 -0700

    Figued out REST api curl commands to delete elements from registry

---
+++ dregistry-get.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IMAGE=$1
+TAG=$2
+REGISTRY=${3:-https://dockerepo.library.arizona.edu:5000}
+
+if [ "$IMAGE" == "" ]; then
+    curl -X GET $REGISTRY/v2/_catalog
+else
+if [ "$TAG" == "" ]; then
+    curl -X GET $REGISTRY/v2/$IMAGE/tags/list
+else
+    curl -X GET -vvv \
+        -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
+        -k $REGISTRY/v2/$IMAGE/manifests/$TAG
+fi
+fi
