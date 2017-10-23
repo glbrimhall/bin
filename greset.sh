5bff26b0abc8bebbab01d9a1e358ed75eb587226
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2017-10-23 11:01:47 -0700

    Added a command to reset git

---
+++ greset.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+ROOT_DIR="`git rev-parse --show-toplevel`"
+
+if [ "$?" == "0" ]; then
+    echo "Not in a git repository, exiting"
+    exit $?
+fi
+
+cd "$ROOT_DIR"
+git reset
+git checkout .
+git clean -fdx
