62582e4ea431e2e685c1a674e0f1c9b3316b2dda
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2017-11-16 08:40:56 -0700

    latest updates to ginstall-glbrimhall.sh #1

--- ginstall-glbrimhall.sh
+++ ginstall-glbrimhall.sh
@@ -3,6 +3,8 @@
 USERNAME=${1:-glbrimhall}
 
 git clone git@github.com:$USERNAME/bin.git
+ln -s /home/$USERNAME/bin/dot.gitconfig-glb-linux /home/$USERNAME/.gitconfig
+
 git clone git@github.com:$USERNAME/dot.emacs.d.git
 ln -s /home/$USERNAME/dot.emacs.d /home/$USERNAME/.emacs.d
 ln -s /home/$USERNAME/dot.emacs.d/dot.emacs /home/$USERNAME/.emacs
