89108f754d605371946fce6954717348980256b3
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-01-11 12:41:14 -0700

    Latest changes to drun.sh to support logging into lightweight alpine images where bash isn't present

--- drun.sh
+++ drun.sh
@@ -9,6 +9,8 @@ echo "TTY: tput cols=$CONSOLE_COLS lines=$CONSOLE_ROWS"
 echo "RUN: stty cols $CONSOLE_COLS rows $CONSOLE_ROWS"
 
 CONTAINER=${1:-$DEFAULT_CONTAINER}
+SHELL=${2:-bash}
+
 docker start "$CONTAINER"
-docker exec -it "$CONTAINER" bash -i
+docker exec -it "$CONTAINER" $SHELL -i
 #docker exec -it "$CONTAINER" bash -c "stty cols $CONSOLE_COLS rows $CONSOLE_ROWS"
