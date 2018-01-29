9e7a5d9e1b80cfd6d23578ac60503af4b48bacff
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-01-29 12:15:47 -0700

    Added script to do aws glacier uploads

--- aws-install.sh
+++ aws-install.sh
@@ -4,4 +4,11 @@ sudo apt-get install -y python-pip
 
 sudo pip install --upgrade pip
 
+# Instructions taken from:
+# https://docs.aws.amazon.com/cli/latest/userguide/awscli-install-linux.html
 sudo pip install awscli --upgrade
+
+# Instructions taken from:
+# https://github.com/awslabs/aws-shell
+sudo pip install aws-shell --upgrade
+sudo pip install glacier-tool --upgrade
