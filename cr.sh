2d3f8def68396382dab8cf301516fb1e9d66c6c4
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-02-14 17:22:08 -0700

    Got good settings for cr.sh

--- cr.sh
+++ cr.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 URL=${1:-http://localhost}
 CHROME=${2:-google-chrome}
+#DATETIME=`date +%Y-%m-%d.%H.%M.%S`
 DATETIME=${3:-"chrome-debug"}
 TMP=/tmp
  
 mkdir -p $TMP/$DATETIME
 
 #$CHROME --auto-open-devtools-for-tabs $URL
+#    --disable-infobars \
+
 $CHROME \
+    --window-position=300,800 \
+    --window-size="1200,1224" \
     --user-data-dir=$TMP/$DATETIME \
-    --disable-infobars \
     --no-default-browser-check \
     --auto-open-devtools-for-tabs \
     -incognito \
-    $URL
+    $URL 2>/dev/null
