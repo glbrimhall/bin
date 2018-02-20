eed280a664560146f8bb08bbd4f9ff19f1c61ec0
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-02-20 16:58:19 -0700

    Added refinement to dstop.sh so it could be used for docker-compose files

--- dstop.sh
+++ dstop.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ -f docker-compose.yml ]; then
+  docker-compose stop
+else
+
 . $HOME/bin/docker-default-container.sh
 
 CONTAINER=${1:-$DEFAULT_CONTAINER}
 docker stop "$CONTAINER"
 docker logs "$CONTAINER"
 
+fi
