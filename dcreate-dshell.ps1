902146c8c078017cf1be84ebe6cbb1755177da06
gbrimhall <geoff@glbrimhall.com>
2018-12-07 16:55:10 -0700

    Fixed docker tag to propery pythondev-stretch

--- dcreate-dshell.ps1
+++ dcreate-dshell.ps1
@@ -2,6 +2,6 @@
 
 #docker run -d --user root -it --entrypoint /bin/bash --restart=always --name dshell-test -v c:/:/win -w=/root debslim-spacevim:stretch 
 
-docker run -d --user root -it --entrypoint /bin/bash --restart=always --name dshell -v c:/:/win -w=/root glbrimhall/debslim:spacevim-stretch 
+docker run -d --user root -it --entrypoint /bin/bash --restart=always --name dshell -v c:/:/win -w=/root glbrimhall/debslim:pythondev-stretch 
 
 #       -w=//root \
