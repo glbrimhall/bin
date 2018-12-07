cdc1ad88c605e85b032fb5919f5bf4ce38f262f3
gbrimhall <geoff@glbrimhall.com>
2018-12-07 15:25:06 -0700

    Getting reproduction of docker dev environment on windows solidified

--- dcreate-dshell.ps1
+++ dcreate-dshell.ps1
@@ -1,5 +1,7 @@
 #!powershell.exe -File c:\\Users\\gbrimhall\\bin\\dubuntu.ps1
 
-docker run -d --user root -it --entrypoint /bin/bash --restart=always --name dshell -v c:/:/win -w=/root glbrimhall/debslim:editors-stretch 
+#docker run -d --user root -it --entrypoint /bin/bash --restart=always --name dshell-test -v c:/:/win -w=/root debslim-spacevim:stretch 
+
+docker run -d --user root -it --entrypoint /bin/bash --restart=always --name dshell -v c:/:/win -w=/root glbrimhall/debslim:spacevim-stretch 
 
 #       -w=//root \
--- dshell-ln.sh
+++ dshell-ln.sh
@@ -15,9 +15,19 @@ mv /home /home.orig
 ln -s /win/Users/ /home
 
 cd /root
-ln -s /home/gbrimhall gbrimhall
+rm -vfr /root/.SpaceVim.d
+ln -vs /home/$USER/.SpaceVim.d/ .SpaceVim.d
+ln -vs /home/$USER $USER
 
-foreach ENTRY in $LNENTRIES; do
-  ln -s /home/$USER/$ENTRY $ENTRY
+for ENTRY in $LNENTRIES; do
+  ln -vs /home/$USER/$ENTRY $ENTRY
 done
+
+cat <<EOF >> /root/.bashrc
+
+stty -ixon
+
+export PATH="/root/bin:$PATH"
+
+EOF
 
