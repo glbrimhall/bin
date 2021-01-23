bc998deb88cf8ced9580fbbfbd37d8bfc7ef2827
Geoffrey L. Brimhall <geoff@glbrimhall.com>
2021-01-23 14:24:17 -0700

    Added script to launch musescore

---
+++ muse.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+MUSESCORE_DPI=106
+
+cd ~/Music/muse
+./Muse* -D $MUSESCORE_DPI &
--- start-macvlan0.sh
+++ start-macvlan0.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/bash
 
-interface=$1
-event=$2
-
 LOG=/home/geoff/bin/start-macvlan.log
 
 date >> $LOG
 echo $1 $2 >> $LOG
+
+sudo /home/geoff/bin/start-macvtap.sh
+
+exit 0
+
+
+interface=$1
+event=$2
 
 if [[ $interface != "eno1" ]] || [[ $event != "up" ]]; then
   exit 0
 fi
 
 # place your commands bellow this line
-sudo /home/geoff/bin/start-macvtap.sh
