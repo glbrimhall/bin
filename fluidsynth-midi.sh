ed1cdec2269298393e313089395d91da344e99b6
Geoffrey L. Brimhall <geoff@glbrimhall.com>
2021-07-23 08:01:59 -0700

    Adding commands for nordvpn linux client to allow local subnet use while in vpn

--- fluidsynth-midi.sh
+++ fluidsynth-midi.sh
@@ -1,2 +1,7 @@
-fluidsynth -a alsa -o audio.alsa.device='hw:0,0' /usr/share/sounds/sf2/FluidR3_GM.sf2 
+#!/bin/sh
+
+SF=/usr/share/sounds/sf3/MuseScore_General_Full.sf3
+#SF=/usr/share/sounds/sf2/FluidR3_GM.sf2
+
+fluidsynth -a alsa -o audio.alsa.device='hw:0' $SF $1 
 #fluidsynth -a alsa -o audio.alsa.device='hw:0,0' /usr/share/sounds/sf2/merlin_vienna.sf2 -g 0.8 $1
