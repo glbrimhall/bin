13650b28887f1c8311a46a8422b683054af803a3
geoff <geoff@BULLSEYE-debian-live-builder>
2021-08-05 08:11:36 -0700

    Moved more commands into nordvpn-setup.sh

--- nordvpn-whitelist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-
-nordvpn whitelist add port 22
-nordvpn whitelist add port 21
-nordvpn whitelist add port 22
-nordvpn whitelist add port 2049
-nordvpn whitelist add subnet 192.168.0.0/24
-
