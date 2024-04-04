cd32548c1eb4ee0b408880010ffdc91bd6c99509
Geoff Brimhall <geoff@glbrimhall.com>
2024-04-03 23:48:13 -0700

    tar-{encrypt,descript}.sh working

--- tar-decrypt.sh
+++ tar-decrypt.sh
@@ -1,3 +1,6 @@
-TARDIR=$1
-tar -vc --atime-preserve -f $TARDIR.tar $TARDIR
-7z a -p $TARDIR.tar.7z $TARDIR.tar
+TARDIR=${1%%.*}
+echo "DECRYPT $1 to $TARDIR"
+
+7z x $TARDIR.tar.7z
+tar -vx --atime-preserve -f $TARDIR.tar
+rm -f $TARDIR.tar
--- tar-encrypt.sh
+++ tar-encrypt.sh
@@ -1,3 +1,6 @@
 TARDIR=$1
-7z x -p $TARDIR.tar.7z
-tar -vx --xz --atime-preserve -f $TARDIR.tar 
+echo "ENCRYPT to $TARDIR to $TARDIR.tar.7z"
+
+tar -vc --atime-preserve -f $TARDIR.tar $TARDIR
+7z a -p $TARDIR.tar.7z $TARDIR.tar
+rm -f $TARDIR.tar
