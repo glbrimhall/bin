840e470f4cea1148e9f404bcc841ed6afd267d34
Geoff Brimhall <geoff@glbrimhall.com>
2024-04-04 00:08:07 -0700

    tar-encrypt.sh enabled streaming

--- tar-decrypt.sh
+++ tar-decrypt.sh
@@ -1,6 +1,9 @@
 TARDIR=${1%%.*}
 echo "DECRYPT $1 to $TARDIR"
 
+# Note using streaming disables password prompt
+# 7z x -p -so $TARDIR.tar.7z | tar -vx --atime-preserve -f -
+
 7z x $TARDIR.tar.7z
 tar -vx --atime-preserve -f $TARDIR.tar
 rm -f $TARDIR.tar
--- tar-encrypt.sh
+++ tar-encrypt.sh
@@ -1,6 +1,4 @@
 TARDIR=$1
 echo "ENCRYPT to $TARDIR to $TARDIR.tar.7z"
 
-tar -vc --atime-preserve -f $TARDIR.tar $TARDIR
-7z a -p $TARDIR.tar.7z $TARDIR.tar
-rm -f $TARDIR.tar
+tar -vc --atime-preserve -f - $TARDIR | 7z a -si -p $TARDIR.tar.7z
