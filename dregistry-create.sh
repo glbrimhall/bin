b5e87ff5b909767139aa802e364e66d310ea227d
Geoff Brimhall <geoff@glbrimhall.com>
2024-05-17 15:32:46 -0700

    got repo-web to work

--- dregistry-create.sh
+++ dregistry-create.sh
@@ -1,6 +1,2 @@
-REPO_NAME=repo-img
-
-docker network create -d bridge repo-img-network
-docker run -d --restart always --network=repo-img-network -p 5000:5000 --name $REPO_NAME -e REGISTRY_STORAGE_DELETE_ENABLED=TRUE registry:2
-docker run -d --restart always --network=repo-img-network -p 8080:8080 --name repo-web -e REGISTRY_URL=http://$REPO_NAME:5000/v2 -e REGISTRY_NAME=$HOSTNAME:5000 hyper/docker-registry-web
-
+docker compose --file=dregistry-create.yaml down
+docker compose --file=dregistry-create.yaml up -d
