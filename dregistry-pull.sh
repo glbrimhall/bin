7c13dd99e9e2b1b620f272da822bd5c45e905796
Geoff Brimhall <geoff@glbrimhall.com>
2024-05-18 01:53:03 -0700

    dregistry-* - switched to https

--- dregistry-pull.sh
+++ dregistry-pull.sh
@@ -1,5 +1,6 @@
 IMG="$1"
 IMG_REPO="$HOSTNAME:5000/$IMG"
 
-docker pull --insecure-registry "$IMG_REPO"
+docker pull "$IMG_REPO"
+#docker pull --insecure-registry "$IMG_REPO"
 
--- dregistry-push.sh
+++ dregistry-push.sh
@@ -3,6 +3,5 @@ IMG_REPO="$HOSTNAME:5000/$IMG"
 
 docker tag "$IMG" "$IMG_REPO"
 docker push "$IMG_REPO"
-#docker push --insecure-registry "$IMG_REPO"
 docker rmi "$IMG_REPO"
 
