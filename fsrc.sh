07e33404adbdb27df3a9f6ae660d7f009fba0318
Geoff Brimhall <geoff@glbrimhall.com>
2025-02-11 10:01:42 -0800

    fsrc.sh: updated to use xargs

--- fsrc.sh
+++ fsrc.sh
@@ -1,3 +1,3 @@
-find . -type f \( -iname \*.h -o -iname \*.hpp -o -iname \*.cpp  \) -exec       grep -nH --color $1 {} \;
+find . -type f \( -iname \*.h -o -iname \*.hpp -o -iname \*.cpp  \) -print0 | xargs -0 grep -nH --color $1
 #find . -type f \( ! -name \*.o ! -name \*.a ! -name index ! -iname sabine      \) -exec grep -nH --color $1 {} \;
 
